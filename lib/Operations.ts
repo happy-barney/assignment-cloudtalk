4447f29626bff60109552c11e979fc5152fbb2fa
Branislav Zahradník <happy.barney@gmail.com>
2024-03-11 08:14:54 +0100

    [api] Operation product_fetch

--- lib/Operations.ts
+++ lib/Operations.ts
@@ -1,4 +1,5 @@
 
 export { product_create } from './Operations/product_create';
 export { product_delete } from './Operations/product_delete';
+export { product_fetch  } from './Operations/product_fetch';
 export { product_list   } from './Operations/product_list';
--- lib/api/v1/paths/product/{product_id}.ts
+++ lib/api/v1/paths/product/{product_id}.ts
@@ -2,9 +2,11 @@
 import { Operation } from 'express-openapi';
 
 import { product_delete } from '../../../../Operations';
+import { product_fetch  } from '../../../../Operations';
 
 export default function () : Record <string, Operation> {
 	return {
 		DELETE: product_delete,
+		GET:    product_fetch,
 	};
 }
